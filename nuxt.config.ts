5d10126c9737261143816dce0bb90e90381699da
Reagan Mahinay <reaganmahinay.web@gmail.com>
2023-12-09 06:10:11 +0800

    Revert

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
 import fsDriver from "unstorage/drivers/fs";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  nitro: {
-   storage: {
-    fs: {
-      driver: 'fs',
-      base: './public'
-    }
-   }
-  }
+  devtools: { enabled: true }
 })
