e1b782a292faf100bea23cc232825cc704ec70ce
Reagan Mahinay <reaganmahinay.web@gmail.com>
2023-12-11 19:50:36 +0800

    Test

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -1,5 +1,6 @@
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
   await useStorage().setItem('fs:storage', query);
+  console.log('Hello World')
   return await useStorage().getItem('fs:storage');
 });
