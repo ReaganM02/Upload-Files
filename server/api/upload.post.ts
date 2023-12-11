496635d23f67719a733d78208d022d1b346fec9c
Reagan Mahinay <reaganmahinay.web@gmail.com>
2023-12-11 18:37:32 +0800

    Test

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -6,12 +6,11 @@ export default defineEventHandler(async(event) => {
   // Use map instead of forEach to create an array of promises
   const uploadedPromises = files?.map(async (file) => {
     await useStorage().setItemRaw(`fs:${file.filename}`, file.data);
-    uploadedFilePaths.push(`/${file.filename}`);
   });
   
   // Use Promise.all to wait for all promises to resolve
   await Promise.all(uploadedPromises!);
   
-  return uploadedFilePaths;
+  return 200;
   
 })
