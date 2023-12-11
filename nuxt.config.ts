068e921a00a9500717ec9837824e961516ec5d1d
Reagan Mahinay <reaganmahinay.web@gmail.com>
2023-12-11 19:20:05 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,10 @@ export default defineNuxtConfig({
       fs: {
         driver: 'fs',
         base: './public'
+      },
+      test: {
+        driver: 'fs',
+        base: './images'
       }
     }
   }
