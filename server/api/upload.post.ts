cbfab409ec5b9413f526f52273f6fe5bda946ff7
Reagan Mahinay <reaganmahinay.web@gmail.com>
2023-12-12 21:41:49 +0800

    Test in production

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -1,7 +1,9 @@
 export default defineEventHandler(async (event) => {
   const files = await readMultipartFormData(event)
+  const uploadFilePaths:string[] = []
   files?.forEach((file) => {
     useStorage().setItemRaw(`fs:${file.filename}`, file.data)
+    uploadFilePaths.push(`/${file.filename}`)
   })
-  return 200
+  return uploadFilePaths
 });
